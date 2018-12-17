b379f23812cd97a02f1b57091093924b49e7c414
Lahbali <ayoublahbali12@gmail.com>
2018-12-17 19:00:34 +0100

    modification du message a afficher dans la classe HelloWorld.java

--- HelloWord/src/HelloWorld.java
+++ HelloWord/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		String message = "Hello world!"
+		String message = "Hello les zéro"
 		System.out.println(message);
 
 	}
